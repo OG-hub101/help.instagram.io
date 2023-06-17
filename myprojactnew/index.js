1e8c93eefcd2d37408c719ff08177dabb8fee79a
OG-hub101 <135167184+OG-hub101@users.noreply.github.com>
2023-06-17 11:08:52 +0100

    Add files via upload

---
+++ myprojactnew/index.js
@@ -0,0 +1,26 @@
+function sendMail(){
+
+
+
+    const btn = document.getElementById('button');
+
+    document.getElementById('form')
+    .addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    btn.value = 'Sending...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_p98gh8q';
+
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+        btn.value = 'Send Email';
+        alert('Sent!');
+        }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+        });
+    });
+}
+
